1ed70bab3d2aa06fff42d57e43fabb37903ca890
FTUBE <tian.yang14@imperial.ac.uk>
2015-12-02 02:52:03 +0800

    fix bugs of pointer deletion regarding strength

--- kankolle/main.cpp
+++ kankolle/main.cpp
@@ -8,6 +8,10 @@
 using namespace std;
 
 const int MAXLENGTH = 100;
+const int COMMLENGTH = 18;
+const int JIKANLENGTH = 256;
+const int TYPELENGTH = 256;
+const int KANMUSULENGTH = 256;
 
 int console();
 void kenzouRecipe();
@@ -16,7 +20,7 @@ void parse(char *line,int &now, char *type, char *kanmusu);
 void getEnseiSearch();
 
 int main(){
-  bool enseiload = false;
+    bool enseiload = false;
   
   while(true){
     int c = console();
@@ -29,7 +33,7 @@ int main(){
     case 2:
       if(!enseiload)loadEnsei();
       getEnseiSearch();
-      break;
+    break;
     case 6:
       kenzouTime();
       break;
@@ -45,7 +49,7 @@ int console(){
   string console_line[] = {"建造レシピ (ken)",
 			   "遠征について (ensei)",
 			   "改造レベル (kai)",
-			   "艦載機 (kan)",
+			   "艦載機（制空/砲撃戦）の最大化方案 (kan)",
 			   "出撃関連 (syu)",
 			   "時間(jikan)"};
   
@@ -53,10 +57,9 @@ int console(){
     cout << console_line[i] << endl;
   }
   
-  char *comm = new char[MAXLENGTH];
+  char *comm = new char[COMMLENGTH+1];
+  cin.getline(comm,COMMLENGTH);
 
-  cin.getline(comm,MAXLENGTH);
-  
   if(strcmp(comm,"ken") == 0){
     delete [] comm;
     return 1;
@@ -82,7 +85,8 @@ int console(){
 
 
 void kenzouRecipe(){
-  
+
+  printf("======================\n");
   printf("駆逐艦:\n30 30 30 30\n");
   printf("250 30 200 30 レア駆逐艦-雪風,島風,Z3(Z1旗艦),Z1(Z3旗艦)のみ\n\n");
   printf("軽巡:\n30 30 30 30\n");
@@ -90,7 +94,7 @@ void kenzouRecipe(){
   printf("重巡:\n250 30 200 30\n\n");
   printf("戦艦:\n400 30 600 30\n\n");
   printf("空母:\n300 30 400 300\n\n");
-  printf("潜水艦:\n250 30 200 30\n\n");
+  printf("潜水艦:\n250 30 200 30\n");
   printf("======================\n\n");
 
   return;
@@ -104,14 +108,14 @@ void kenzouTime(){
   ifstream in;
   in.open("jikan.data");
 
-  char *line = new char[256];
-  char *type = new char[56];
-  char *kanmusu = new char[56];
+  char *line = new char[JIKANLENGTH];
+  char *type = new char[TYPELENGTH];
+  char *kanmusu = new char[KANMUSULENGTH];
   
   int now;
   char nextcomm;
   
-  in.getline(line,256);
+  in.getline(line,JIKANLENGTH);
   parse(line,now,type,kanmusu);
   printf("\n");
   if(now == time){
@@ -122,9 +126,12 @@ void kenzouTime(){
     if(now > time) break;
     if(now == time) printf("----->%d, %s , %s\n\n",now,type,kanmusu);
   }
+  
   delete []line;
-  delete []type;
   delete []kanmusu;
+  delete []type;
+  
+  in.close();
 }
 
 void parse(char *line,int &now, char *type, char *kanmusu){
